3158f1861ce772560d85373938fe7614ad362386
Christopher Wiens <cwiens1@gmail.com>
2019-07-17 18:27:33 -0400

    Added more documentation

--- backend/controllers/image.py
+++ backend/controllers/image.py
@@ -11,9 +11,16 @@ from constants import FULL, THUMBNAIL, ORIGINAL
 
 image_page = Blueprint('image_page', __name__, template_folder='templates')
 
+# TODO: Look into reusing function to be more fluid with different formats of saved images
+
 
 @image_page.route('/<img_id>/thumbnail', methods=['GET'])
 def get_thumbnail_image(img_id):
+    """
+    Gets the thumbnail of a specific image
+    :param img_id: uuid of image
+    :return: Response with byte stream of image
+    """
     image = Image.query.filter(and_(Image.uuid == img_id, Image.size == THUMBNAIL)).first_or_404()
     resp = Response(open(image.path, "rb"))
     resp.headers.set('Content-Type', 'image/jpeg')
@@ -22,6 +29,11 @@ def get_thumbnail_image(img_id):
 
 @image_page.route('/<img_id>/full-size', methods=['GET'])
 def get_full_size_image(img_id):
+    """
+    Gets the full-size representation of a specific image in jpeg format
+    :param img_id: uuid of image
+    :return: Response with byte stream of image
+    """
     image = Image.query.filter(and_(Image.uuid == img_id, Image.size == FULL)).first_or_404()
     resp = Response(open(image.path, "rb"))
     resp.headers.set('Content-Type', 'image/jpeg')
@@ -30,6 +42,11 @@ def get_full_size_image(img_id):
 
 @image_page.route('/<img_id>/original', methods=['GET'])
 def get_original_image(img_id):
+    """
+    Gets the original image in whatever format it was saved in
+    :param img_id: uuid of image
+    :return: Response with byte stream of image
+    """
     image = Image.query.filter(and_(Image.uuid == img_id, Image.size == ORIGINAL)).first_or_404()
     resp = Response(open(image.path, "rb"))
     resp.headers.set('Content-Type', 'image/{0}'.format(image.image_type.lower()))
@@ -38,6 +55,11 @@ def get_original_image(img_id):
 
 @image_page.route('/<img_id>/metadata', methods=['GET'])
 def get_image_metadata(img_id):
+    """
+    Gets the metadata associated with the image that is saved in the database
+    :param img_id: uuid of image
+    :return: Json of metadata
+    """
     image = MasterRecord.query.filter(MasterRecord.uuid == img_id).first_or_404()
     resp = Response(json.dumps(image.to_json(), cls=DateTimeEncoder))
     resp.headers.set('Content-Type', 'application/json')
@@ -46,6 +68,10 @@ def get_image_metadata(img_id):
 
 @image_page.route('', methods=['GET'])
 def get_images():
+    """
+    Gets list of UUIDs of images between two times, if no time is supplied, it returns all.
+    :return:
+    """
     begin_time = parse_time(request.args.get('begin-time', '1980-01-01'))
     end_time = parse_time(request.args.get('end-time', '2099-01-01'))
 
